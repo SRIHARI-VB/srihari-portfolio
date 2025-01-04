da0d864b778a0460f2333c79116a44825a00b3c3
SRIHARI-VB <srihari.vb.kovai@gmail.com>
2025-01-04 21:41:51 +0530

    updated: resume url

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -5,7 +5,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/SRIHARI-VB",
   resume:
-    "https://drive.google.com/file/d/1tsFjPmIVoVa5X_hPbqGw0f2WOL-sph5H/view?usp=sharing",
+    "https://drive.google.com/file/d/1S6Cv-wvf1HiYXDvWBbBr0eE3yzHp0QWD/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/vb-srihari",
   twitter: "https://x.com/__srihari__vb",
   insta: "https://www.instagram.com/__._.srihari._.__",
